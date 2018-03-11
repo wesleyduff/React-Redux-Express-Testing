ff505bcfc5a3829c890b98058356dc52ef906aef
Wes A. Duff <slysop@gmail.com>
2018-03-11 09:52:39 -0500

    all register actions to this point are tested

--- client/src/_actions/__tests__/registActions.jest.test.js
+++ client/src/_actions/__tests__/registActions.jest.test.js
@@ -1,11 +1,12 @@
 import * as ActionTypes from '../../_constants/actionTypes';
 import * as ActionCreators from '../registerActions';
+import { ENODATA } from 'constants';
 
 describe('Actions', () => {
     let username = "fake@fake.com";
     let password = "@Password1";
 
-    it("Should create a registered user", () => {
+    it("Should start the process of registering a user", () => {
         const dispatch = jest.fn();
         const expected = {
             type: ActionTypes.AWS_SIGN_UP_SAVING,
@@ -20,6 +21,58 @@ describe('Actions', () => {
 
         //finally assert that the dispatch was called with our expected action
         expect(dispatch).toBeCalledWith(expected);
+    });
+
+    it("should create an action to fail a signup", () => {
+        const data =  {errorCode: 500, message: 'User already exists'};
+        const expected = {
+            type: ActionTypes.AWS_SIGN_UP_FAIL,
+            data: data
+        }
+
+       const actual = ActionCreators.awsSignUpUserFailed(data);
+        
+       expect(actual).toEqual(expected);
+
+    });
+
+    it("should create an action to signup a user", () => {
+        const data = {cognito_user: 'fake@fake.com'};
+        const expected = {
+            type: ActionTypes.AWS_SIGN_UP_SUCCESS,
+            data
+        }
+
+       const actual = ActionCreators.awsSignUpUserSuccess(data);
+        
+       expect(actual).toEqual(expected);
+
+    });
+
+    it("should create an action to throw an error", () => {
+        const data = {error: {code:500, mesage: 'Something went wrong'}};
+        const expected = {
+            type: ActionTypes.THROW_ERROR,
+            data
+        }
+
+       const actual = ActionCreators.throwError(data);
+        
+       expect(actual).toEqual(expected);
+
+    });
+
+    it("should create an action to clear an error", () => {
+        const data = {error: null}
+        const expected = {
+            type: ActionTypes.CLEAR_ERROR,
+            data
+        }
+
+       const actual = ActionCreators.clearError(data);
+        
+       expect(actual).toEqual(expected);
+
     });
 
 });
--- client/src/_actions/registerActions.js
+++ client/src/_actions/registerActions.js
@@ -2,7 +2,9 @@
 import {
     THROW_ERROR, 
     CLEAR_ERROR,
-    AWS_SIGN_UP_SAVING
+    AWS_SIGN_UP_SAVING,
+    AWS_SIGN_UP_FAIL,
+    AWS_SIGN_UP_SUCCESS
 } from '../_constants/actionTypes.js'
 
 export function throwError(data) {
@@ -17,12 +19,24 @@ export function clearError(data) {
             data
         }
     };
-export function awsSigningUpUser(data) {
+export function awsSignUpUser(data) {
         return {
             type: AWS_SIGN_UP_SAVING,
             data
         }
     };
+export function awsSignUpUserFailed(data){
+        return {
+            type: AWS_SIGN_UP_FAIL,
+            data
+        }
+}
+export function awsSignUpUserSuccess(data){
+    return {
+        type: AWS_SIGN_UP_SUCCESS,
+        data
+    }
+}
 
 
 /** 
@@ -30,9 +44,7 @@ export function awsSigningUpUser(data) {
  */
 export function AWS_SignUpUser(username, password) {
     return (dispatch => {
-        return dispatch({
-            type: AWS_SIGN_UP_SAVING,
-            data: {username, password}
-        });
+        const data = {username, password};
+        return dispatch(this.awsSignUpUser(data));
     });
 };
