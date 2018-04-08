ebeeb2e624fed3703fdb76c48f6c4e37ee47d014
Wes A. Duff <slysop@gmail.com>
2018-04-08 09:45:24 -0500

    Added action type for setting user'

--- client/src/_constants/actionTypes.js
+++ client/src/_constants/actionTypes.js
@@ -4,3 +4,4 @@ export   const AWS_SIGN_UP_FAIL = 'AWS_SIGN_UP_FAIL';
 export   const AWS_SIGN_UP_SUCCESS = 'AWS_SIGN_UP_SUCCESS';
 export   const THROW_ERROR = 'THROW_ERROR';
 export   const CLEAR_ERROR = 'CLEAR_ERROR';
+export   const SET_USER = 'SET_USER';
